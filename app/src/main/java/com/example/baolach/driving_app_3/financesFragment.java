c8c2280a5b12cf702ce697e1d67d73a13377b843
baolach <c13367911@mydit.ie>
2017-02-02 16:29:46 +0000

    working with finances page

---
+++ app/src/main/java/com/example/baolach/driving_app_3/financesFragment.java
@@ -0,0 +1,22 @@
+package com.example.baolach.driving_app_3;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class financesFragment extends Fragment {
+
+    public financesFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_finances, container, false);
+    }
+}
