f9de6696fd6f412dbc259b984245ac4cc7788693
baolach <c13367911@mydit.ie>
2017-03-25 23:08:44 +0000

    checkpoint - trying to get Finance.java independent

---
+++ app/src/main/java/com/example/baolach/driving_app_3/FinanceAdapter.java
@@ -0,0 +1,86 @@
+package com.example.baolach.driving_app_3;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Baolach on 04/02/2017.
+ */
+
+public class FinanceAdapter extends BaseAdapter {
+
+    private Context context;
+    private int layout;
+    private ArrayList<Client> clientlist;
+
+    public FinanceAdapter(Context context, int layout, ArrayList<Client> clientlist) {
+        this.context = context;
+        this.layout = layout;
+        this.clientlist = clientlist;
+    }
+
+    @Override
+    public int getCount() {
+
+        return clientlist.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return clientlist.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+
+    private class ViewHolder{
+        TextView client_name,client_phone, client_address, log_no, driver_no, dob, no_of_lessons, balance_due, comments ;
+    }
+
+    @Override
+    public View getView(int position, View view, ViewGroup parent) {
+
+        View row = view;
+        ViewHolder holder = new ViewHolder();
+
+        if(row == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            row = inflater.inflate(layout, null);
+            holder.client_name = (TextView) row.findViewById(R.id.client_name);
+            holder.client_phone = (TextView) row.findViewById(R.id.client_phone);
+            holder.client_address = (TextView) row.findViewById(R.id.client_address);
+            holder.log_no = (TextView) row.findViewById(R.id.log_no);
+            holder.driver_no = (TextView) row.findViewById(R.id.driver_no);
+            holder.dob = (TextView) row.findViewById(R.id.dob);
+            holder.no_of_lessons = (TextView) row.findViewById(R.id.no_of_lessons);
+            holder.balance_due = (TextView) row.findViewById(R.id.balance_due);
+            holder.comments = (TextView) row.findViewById(R.id.comments);
+
+
+
+            row.setTag(holder);
+        }
+        else
+        {
+            holder = (ViewHolder) row.getTag();
+        }
+
+
+        Client client = clientlist.get(position);
+        holder.client_name.setText(client.getName());
+        holder.log_no.setText(client.getLogno());
+        //holder.client_address.setText(client.getAddress());
+
+
+        return row;
+    }
+}
--- app/src/main/java/com/example/baolach/driving_app_3/finances.java
+++ app/src/main/java/com/example/baolach/driving_app_3/finances.java
@@ -1,12 +1,29 @@
 package com.example.baolach.driving_app_3;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
-import android.database.Cursor;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
 
 /*
 when finances button is clicked on mainActivity - the FinancesCursorAdapter is loaded which lists
@@ -25,99 +42,346 @@ list_finances.xml is connect to Finances.java Activity but lists the clientnames
 */
 public class Finances extends Activity
 {
-    DBManager db = new DBManager(this);
+
+    static ArrayList<String> clientsName = new ArrayList<String>();
+    static ArrayList<String> clientsPhone = new ArrayList<String>();
+    static ArrayList<String> clientsAddress = new ArrayList<String>();
+    static ArrayList<String> clientsLogNo = new ArrayList<String>();
+    static ArrayList<String> clientsDriverNo = new ArrayList<String>();
+    static ArrayList<String> dateOfBirth = new ArrayList<String>();
+    static ArrayList<String> noOfLessons = new ArrayList<String>();
+    static ArrayList<String> balanceDue = new ArrayList<String>();
+    static ArrayList<String> clientComments = new ArrayList<String>();
+
+    ListView listView;
+    ArrayList<Client> list;
+    FinanceAdapter adapter = null;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.list_finances);
+        setContentView(R.layout.list_finances); // shows the listView in ListClients activity
 
-        final ListView listView = (ListView) findViewById(R.id.listView_clients); // in the list_clients xml
-        try {
-            db.open();
-            Cursor result = db.getAll();
-            FinancesCursorAdapter cursorAdapter = new FinancesCursorAdapter(Finances.this, result);
-            listView.setAdapter(cursorAdapter);
-            
-            db.close();
-        } catch (Exception e) {
-            e.printStackTrace();
+        // creates variables to be shown and interacted with in the activity
+        // sets up listView and Adapter to accept the data from the urlListView listView = (ListView) findViewById(R.id.listView_clients);
+        listView = (ListView) findViewById(R.id.listView_clients); // the listview ID in list_clients.xml
+        list = new ArrayList<>();
+        adapter = new FinanceAdapter(this, R.layout.client, list); // this sets adapter to the ClientAdapter which uses client.xml
+        listView.setAdapter(adapter); // makes the listview in ListCLients activity output the adapter within the listView
 
+        String url = "http://138.68.141.18:8006/clients/?format=json"; //urlText.getText().toString();
+        ConnectivityManager connmgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connmgr.getActiveNetworkInfo();
+
+        if (networkInfo != null && networkInfo.isConnected()) {
+            new DownloadWebPageTask().execute(url);
+        } else {
+            System.out.println("No network available");
         }
 
-        // these below need to be added also
-
-        // When a client is clicked it goes to the ClientInfo activity and displays all info on that client
+        // the list is generated and the variables added in onPostExecuted. Then if items cliked they are sent with the intent
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long arg) {
-                try {
-//                    Cursor myCursor = (Cursor) parent.getItemAtPosition(position); // where the info is stored on what you clicked
-//                    String lessonname = myCursor.getString(1); // 4th position in the clients table (LOG NUMBER)
-//                    String lessondate = myCursor.getString(2);
-//                    String lessontime = myCursor.getString(3);
-//                    String lessonlocation = myCursor.getString(4);
-//                    String nooflessons = myCursor.getString(5);
-//                    String lessoncomments = myCursor.getString(6);
-//
-//                    Intent i = new Intent(Finances.this, LessonInfo.class);
-//
-//                    i.putExtra("thelessonname", lessonname);
-//                    i.putExtra("thelessondate", lessondate);
-//                    i.putExtra("thelessontime", lessontime);
-//                    i.putExtra("thelessonlocation", lessonlocation);
-//                    i.putExtra("thelessonlocation", nooflessons);
-//                    i.putExtra("thelessoncomments", lessoncomments);
-                    Cursor myCursor = (Cursor) parent.getItemAtPosition(position); // where the info is stored on what you clicked
-                    String theclientsname = myCursor.getString(1); // 4th position in the clients table (LOG NUMBER)
-                    String theclientsphone = myCursor.getString(2);
-                    String theclientsaddress = myCursor.getString(3);
-                    String theclientslognumber = myCursor.getString(4);
-                    String theclientsdrivernumber = myCursor.getString(5);
-                    String theclientsdob = myCursor.getString(6);
-                    String nooflessons = myCursor.getString(7);
-                    String theclientscomments = myCursor.getString(8);
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                int itemId = (int) id;
+                //String name = clientsName.get(itemId);
 
-                    Intent i = new Intent(Finances.this, ClientInfo.class);
+                String clientname = clientsName.get(itemId);
+                String phone = clientsPhone.get(itemId);
+                String address = clientsAddress.get(itemId);
+                String log = clientsLogNo.get(itemId);
+                String driver = clientsDriverNo.get(itemId);
+                String dob = dateOfBirth.get(itemId);
+                String lessons = noOfLessons.get(itemId);
+                String balance = balanceDue.get(itemId);
+                String comments = clientComments.get(itemId);
 
-                    i.putExtra("theclientsname", theclientsname);
-                    i.putExtra("theclientsphone", theclientsphone);
-                    i.putExtra("theclientsaddress", theclientsaddress);
-                    i.putExtra("theclientslognumber", theclientslognumber);
-                    i.putExtra("theclientsdrivernumber", theclientsdrivernumber);
-                    i.putExtra("theclientsdob", theclientsdob);
-                    i.putExtra("nooflessons", nooflessons);
-                    i.putExtra("theclientscomments", theclientscomments);
 
-                    startActivity(i);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+
+                // creates new intent and sends over the client information when item is clicked
+                Intent i = new Intent(Finances.this, ClientInfo.class);
+                i.putExtra("theclientname", clientname);
+                i.putExtra("theclientphone", phone);
+                i.putExtra("theclientaddress", address);
+                i.putExtra("thelognumber", log);
+                i.putExtra("thedriverno", driver);
+                i.putExtra("thedob", dob);
+                i.putExtra("thenumberoflessons", lessons);
+                i.putExtra("thebalance", balance);
+                i.putExtra("theclientscomments", comments);
+
+                startActivity(i);
+
             }
         });
 
     }
 
+    // works in the background
+    private class DownloadWebPageTask extends AsyncTask<String, Void, String> {
 
-//    public void listLessonName(View view) {
-//        try {
-//            Intent lesson_name_intent = new Intent(this, InsertLesson.class);
-//            startActivity(lesson_name_intent);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                System.out.println("DOWNLOAD URL: " + params[0]);
+                return downloadURL(params[0]);
+            } catch (IOException e) {
+                return "Unable to retrieve web page. Check your URL";
+            }
+        }
 
 
-    public void back_screen_btn(View view) {
+        // connects to send the GET request
+        // this is executed second then onPostExectute
+        private String downloadURL(String myurl) throws IOException {
+            InputStream is = null;
+            int len = 5000;
+
+            try {
+                URL url = new URL(myurl);
+
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(10000);
+                conn.setConnectTimeout(15000);
+                conn.setRequestMethod("GET");
+                conn.setDoInput(true);
+
+                conn.connect();
+                int response = conn.getResponseCode();
+                Log.d("DEBUG", "Response code is " + response);
+
+                is = conn.getInputStream();
+                //System.out.println(" ########### input stream: " + is);
+
+
+                String content = parse(is, len);
+                // System.out.println(" ########### content: " + content); // this is the content of the url - ie. the data - this is passed to onPostExectute as result
+
+                return content;
+
+            } finally {
+                if (is != null)
+                    is.close();
+            }
+        }
+
+        protected void onPostExecute(String result) {
+            String text = "";
+
+            try {
+                System.out.println("#####");
+                System.out.println(result); // result is the data string
+                System.out.println("#####");
+
+                JSONArray json = new JSONArray(result);
+
+                // this forloop gets the data into the JSONArray json
+                for (int i = 0; i < json.length(); i++) {
+                    try {
+
+                        int g = 0;// locates the position in the array for the list item
+                        JSONObject object = json.getJSONObject(i); // reads the array into the JSONOBject object
+                        text += "Name: " + object.getString("client_name") + ", Phone: \"" + object.getString("client_phone") + "\", Address: " + object.getString("client_address") + "\n\n";
+                        String clientname = object.optString("client_name").toString();
+                        String clientphone = object.optString("client_phone").toString();
+                        String clientaddress = object.optString("client_address").toString();
+                        String logno = object.optString("log_no").toString();
+                        String driverno = object.optString("driver_no").toString();
+                        String dob = object.optString("dob").toString();
+                        String nooflessons = object.optString("no_of_lessons").toString();
+                        String balancedue = object.optString("balance_due").toString();
+                        String comments = object.optString("comments").toString();
+
+                        // adds to the array list
+                        clientsName.add(clientname);
+                        clientsPhone.add(clientphone);
+                        clientsAddress.add(clientaddress);
+                        clientsLogNo.add(logno);
+                        clientsDriverNo .add(driverno);
+                        dateOfBirth.add(dob);
+                        noOfLessons.add(nooflessons);
+                        balanceDue.add(balancedue);
+                        clientComments.add(comments);
+                        list.add(new Client(g, clientname, clientphone, clientaddress, logno, driverno, dob, nooflessons, balancedue, comments ));
+                        adapter.notifyDataSetChanged();
+                        g++;
+
+
+                        System.out.println("##### ListClients.java");
+                        System.out.println("##### clientname: " + clientname);
+                        System.out.println("##### clientphone: " + clientphone);
+                        System.out.println("##### clientaddress: " + clientaddress);
+                        System.out.println("##### log no: " + logno);
+
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }// end onCreate
+
+        }
+    }
+
+    private String parse(InputStream is, int len) throws IOException, UnsupportedEncodingException {
+        return readIt(is);
+    }
+
+    private String readIt(InputStream is) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+
+        String line = null;
         try {
-            Intent adminActivity = new Intent(this, MainActivity.class);
-            startActivity(adminActivity);
+            while ((line = reader.readLine()) != null) {
+                sb.append(line).append('\n');
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return sb.toString();
+    }
+
+
+
+
+
+
+
+    public void listClientName(View view)
+    {
+        try {
+            Intent client_name_intent = new Intent(this, InsertClient.class);
+            startActivity(client_name_intent);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void goBackScreen(View view) {
+
+        try {
+            Intent lastScreen = new Intent(this, MainActivity.class);
+            startActivity(lastScreen);
         } catch(Exception e) {
             e.printStackTrace();
         }
     }
+
+
+
+
+
+
+
+
+
+
+    // old finances
+//    DBManager db = new DBManager(this);
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState)
+//    {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.list_finances);
+//
+//        final ListView listView = (ListView) findViewById(R.id.listView_clients); // in the list_clients xml
+//        try {
+//            db.open();
+//            Cursor result = db.getAll();
+//            FinancesCursorAdapter cursorAdapter = new FinancesCursorAdapter(Finances.this, result);
+//            listView.setAdapter(cursorAdapter);
+//
+//            db.close();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//
+//        }
+//
+//        // these below need to be added also
+//
+//        // When a client is clicked it goes to the ClientInfo activity and displays all info on that client
+//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long arg) {
+//                try {
+////                    Cursor myCursor = (Cursor) parent.getItemAtPosition(position); // where the info is stored on what you clicked
+////                    String lessonname = myCursor.getString(1); // 4th position in the clients table (LOG NUMBER)
+////                    String lessondate = myCursor.getString(2);
+////                    String lessontime = myCursor.getString(3);
+////                    String lessonlocation = myCursor.getString(4);
+////                    String nooflessons = myCursor.getString(5);
+////                    String lessoncomments = myCursor.getString(6);
+////
+////                    Intent i = new Intent(Finances.this, LessonInfo.class);
+////
+////                    i.putExtra("thelessonname", lessonname);
+////                    i.putExtra("thelessondate", lessondate);
+////                    i.putExtra("thelessontime", lessontime);
+////                    i.putExtra("thelessonlocation", lessonlocation);
+////                    i.putExtra("thelessonlocation", nooflessons);
+////                    i.putExtra("thelessoncomments", lessoncomments);
+//                    Cursor myCursor = (Cursor) parent.getItemAtPosition(position); // where the info is stored on what you clicked
+//                    String theclientsname = myCursor.getString(1); // 4th position in the clients table (LOG NUMBER)
+//                    String theclientsphone = myCursor.getString(2);
+//                    String theclientsaddress = myCursor.getString(3);
+//                    String theclientslognumber = myCursor.getString(4);
+//                    String theclientsdrivernumber = myCursor.getString(5);
+//                    String theclientsdob = myCursor.getString(6);
+//                    String nooflessons = myCursor.getString(7);
+//                    String theclientscomments = myCursor.getString(8);
+//
+//                    Intent i = new Intent(Finances.this, ClientInfo.class);
+//
+//                    i.putExtra("theclientsname", theclientsname);
+//                    i.putExtra("theclientsphone", theclientsphone);
+//                    i.putExtra("theclientsaddress", theclientsaddress);
+//                    i.putExtra("theclientslognumber", theclientslognumber);
+//                    i.putExtra("theclientsdrivernumber", theclientsdrivernumber);
+//                    i.putExtra("theclientsdob", theclientsdob);
+//                    i.putExtra("nooflessons", nooflessons);
+//                    i.putExtra("theclientscomments", theclientscomments);
+//
+//                    startActivity(i);
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        });
+//
+//    }
+//
+//
+////    public void listLessonName(View view) {
+////        try {
+////            Intent lesson_name_intent = new Intent(this, InsertLesson.class);
+////            startActivity(lesson_name_intent);
+////        } catch (Exception e) {
+////            e.printStackTrace();
+////        }
+////    }
+//
+//
+//    public void back_screen_btn(View view) {
+//        try {
+//            Intent adminActivity = new Intent(this, MainActivity.class);
+//            startActivity(adminActivity);
+//        } catch(Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
 
 
 
